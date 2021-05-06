3dc98852701f78321b6a1ba35b58281bfe4fe6fa
Veer <40777269+V3RS@users.noreply.github.com>
2021-05-06 13:33:04 -0400

    seperate file for card class

---
+++ deck/deck.js
@@ -0,0 +1,55 @@
+const Card = require("./card");
+
+class Deck {
+  constructor() {
+    this.cards = [];
+  }
+
+  createDeck() {
+    const suits = ["clubs", "hearts", "spades", "diamonds"];
+    // ace will be high
+    const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    // creates cards for all different suit
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+        this.cards.push(
+          new Card(
+            suits[i],
+            values[j],
+            `/deck/card-svgs/${values[j]}_of_${suits[i]}.svg`
+          )
+        );
+      }
+    }
+    return this.cards;
+  }
+
+  shuffle() {
+    // shuffles the deck by creating a temp variable and getting a random number for the new index
+    // iterating through every card will make sure that every card will be in a new spot
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      const temp = this.cards[i];
+      this.cards[i] = this.cards[rand];
+      this.cards[rand] = temp;
+    }
+    return this.cards;
+  }
+
+  deal() {
+    if (this.cards.length) return this.cards.pop();
+  }
+}
+
+//! To test all functionality for the class
+// const d = new Deck();
+// d.createDeck();
+// console.log("-------------");
+// console.log(d.cards);
+// d.shuffle();
+// console.log("-------------");
+// console.log(d.cards);
+// console.log(d.cards.length);
+// console.log(d.deal());
+// console.log(d.cards.length);
