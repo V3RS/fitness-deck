113f6da29b5b9de4cbb6d87c32e1aff7edf83586
Veer <40777269+V3RS@users.noreply.github.com>
2021-05-09 02:23:30 -0400

    splash page design setup

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
 import cardBack from "../../deck/card-back.png";
-import draw from "./card-draw.svg";
+import drawBtn from "./card-draw.svg";
 
 export default function Main({ deck, setDeck }) {
   const [card, setCard] = useState(deck[deck.cards.length - 1]);
-  const [timer, setTimer] = useState(60);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setTimer(timer - 1);
-    }, [1000]);
-  }, [timer]);
+  const draw = () => {
+    setCard(deck.deal());
+    setDeck(deck);
+    // document.querySelector(".deck").classList.add("deck__transition");
+    // setTimeout(() => {
+    //   document.querySelector(".deck").classList.add("deck__display");
+    // }, 3000);
+  };
 
-  // screen to gif
+  // function to change the exercise and gif depending on the suit
   const bodyweight = (card) => {
     switch (card?.suit) {
       case "clubs":
@@ -33,35 +35,20 @@ export default function Main({ deck, setDeck }) {
         {" "}
         {card?.value} {bodyweight(card)?.exercise}
       </div>
-      <div className="cards__left">
-        {deck.cards.length} card
-        {deck.cards.length > 1 || deck.cards.length === 0 ? "s" : ""} left
-      </div>
+      <div className="cards__left">Cards Left: {deck.cards.length}</div>
       <img src={card?.image} className="card" />
       {card && <img src={bodyweight(card)?.gif} className="exercise" />}
       {deck.cards.length && (
         <span>
-          <img
-            src={cardBack}
-            className="deck"
-            onClick={() => {
-              setCard(deck.deal());
-              setDeck(deck);
-              document.querySelector(".deck").classList.add("deck__transition");
-              setTimeout(() => {
-                document.querySelector(".deck").classList.add("deck__display");
-              }, 3000);
-              // console.log(deck);
-            }}
-          />
+          <img src={cardBack} className="deck" onClick={draw} />
           {deck.cards.length > 1 && <img src={cardBack} className="deck2" />}
         </span>
       )}
 
-      <div className="card__timer"> {timer} </div>
+      {/* <div className="card__timer"> {timer} </div> */}
 
       <div className="card__page__btns">
-        <img src={draw} className="draw__btn" />
+        <img src={drawBtn} className="draw__btn" />
         <button>
           <i className="fas fa-play"></i>
         </button>
--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -5,13 +5,23 @@ import FlipCard from "../FlipCard";
 export default function Splash() {
   return (
     <div className="splash__c">
-      {/* <div>TEST SPLASH</div> */}
-      <div className="flip__card__c">
-        <FlipCard />
+      <div className="splash__left__c">
+        <div className="splash__intro">INTRODUCING</div>
+        <div className="splash__title">Fitness Deck</div>
+        <div className="splash__description">
+          A fun way to stay active with using playing cards! Get started by
+          hovering over the Fitness Deck.
+        </div>
+        <a className="meet__the__dev">MEET THE DEV</a>
       </div>
+      <div className="splash__right__c">
+        <div className="flip__card__c">
+          <FlipCard />
+        </div>
+      </div>
+      {/* <div>TEST SPLASH</div> */}
     </div>
   );
 }
 
-// https://www.sport-fitness-advisor.com/abdominalexercises.html
 // add an ab deck
